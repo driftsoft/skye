abc9bcbd8338c282a1e8769865a2399b6a3acae3
Fred <36894883+xtrp@users.noreply.github.com>
2018-03-08 19:32:43 +0000

    major edits and bug fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+const remote = require('electron').remote;
+
+$("#tabs section .minimize").on("click",function(){
+	var window = remote.getCurrentWindow();
+	window.minimize();	
+});
+
+$("#tabs section .minimize").on("click",function(){
+	alert("bruh");
+	var window = remote.getCurrentWindow();
+	if (!window.isMaximized()) {
+		window.maximize();
+	} else {
+		window.unmaximize();
+	}
+});
+
+$("#tabs section .close").on("click",function(){
+	var window = remote.getCurrentWindow();
+	window.close();
+});
+
 document.getElementById("searchEntry").onfocus=function(){
 	this.select();
 }
